60563089f0f21ff19d9c844f334ba1c2e2c773bf
dj.usagi <sean.hatanaka@gmail.com>
2017-11-03 16:15:01 +0900

    created files for heroku

--- instapic/settings.py
+++ instapic/settings.py
@@ -125,16 +125,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
-
-AUTHENTICATION_BACKENDS = ( 'instapic.authb.AuthB', )
 
 
 # Static files (CSS, JavaScript, Images)
@@ -154,3 +144,6 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+AUTHENTICATION_BACKENDS = ( 'instapic.authb.AuthB', )
